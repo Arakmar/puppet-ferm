78578b03728b34294aed2f618a0c1edfd3b00d2e
Yoann Laissus <yoann.laissus@jeresiliemoncontrat.com>
2024-08-08 17:12:13 +0200

    Replace legacy facts with structured facts

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,7 +6,7 @@ class ferm::params {
   $default_allow_icmp = true
   $default_allow_forwarding = true
 
-  case $::osfamily {
+  case $facts['os']['family'] {
     'Debian': {
       $ferm_config = '/etc/ferm/ferm.conf'
     }
